b492054e464620ad572fe6211cfc252f1867b0c1
Ivan Martinez <ivancrack1212@gmail.com>
2026-02-13 17:03:07 +0100

    T5

--- Tarea5/Trabajo5.js
+++ Tarea5/Trabajo5.js
@@ -7,10 +7,20 @@ let cont = 1;
 
 function norefresh(){}
 
-btn.addEventListener("click",(event)=>{
-    event.preventDefault();
+btn.addEventListener("click",(event1)=>{
+    event1.preventDefault();
     const li = document.createElement("li");
     li.textContent = tarea.value + " (" + subsistema.value + ")";
     lista.appendChild(li);
     tarea.value = null
+});
+
+li.addEventListener("click",(event2)=>{
+    event2.preventDefault();
+    getElementById("listado");
+    li.remove;
+});
+
+li.addEventListener("mouseover",()=>{
+    li.classList.add(rojo);
 });
